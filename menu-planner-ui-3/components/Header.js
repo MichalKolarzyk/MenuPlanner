a679c41646b780b174ca96c1e50aeac5378b4566
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-01-10 21:20:40 +0100

    Fix bug

---
+++ menu-planner-ui-3/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between mx-4'>
+      <div className='w-full rounded-t-lg border border-black px-2 pb-2'>
+        <span>Plan</span>
+      </div>
+      <div className='w-full border-r-2 border-black px-2 mb-2'>
+        <span>Plan</span>
+      </div>
+      <div className='w-full border-r-2 border-black px-2 mb-2'>
+        <span>Plan</span>
+      </div>
+      <div className='w-full border-r-2 border-black px-2 mb-2'>
+        <span>Plan</span>
+      </div>
+      <div className='w-full border-r-2 border-black px-2 mb-2'>
+        <span>Plan</span>
+      </div>
+    </div>
+  )
+}
+
+export default Header
