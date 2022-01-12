00faa9e0ebd7959407fd26f923e83d33f72c0af2
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-01-12 20:26:33 +0100

    Add Header and Plan

--- menu-planner-ui-cra/src/App.js
+++ menu-planner-ui-cra/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import React from "react";
+import Header from './components/Header';
+import Plan from './components/Plan';
 
 function App() {
   return (
     <div>
-
+      <div className='p-5 h-screen'>
+        <Header />
+        <Plan />
+      </div>
     </div>
   );
 }
